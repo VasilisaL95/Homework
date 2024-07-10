aec018805710caba0b73b4dbccc0f373977c60b9
VasilisaL95 <silisochka@mail.ru>
2024-07-11 01:38:34 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+
+quantity_task = 12
+quantity_hour = 1.5
+name_course = 'Python'
+time_for_one = quantity_hour / quantity_task
+print('Курс:', name_course, ', всего задач:',quantity_task, ', затрачено часов:', quantity_hour, ', среднее время выполнения', time_for_one, 'часа.')
