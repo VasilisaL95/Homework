67f16f6f1451ccf076979a99d76951e51d3746f0
VasilisaL95 <silisochka@mail.ru>
2024-07-12 23:35:31 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -5,6 +5,6 @@ print('Immutable tuple:', immutable_var)
 #immutable_var[1] = 45
 #print(immutable_var)
 # кортеж - неизменяемый тип данных
-mutable_list = ([345, 346], 7, 8)
+mutable_list = [[345, 346], 7, 8, 9]
 mutable_list [0][1] = 6
 print('Mutable list:', mutable_list)
